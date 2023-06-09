0c6bcbc44675a715d340a09ff809cc1fc51db9e8
jithin k sunil <jithinksunil96@gmail.com>
2023-06-09 19:03:57 +0530

    folder structure changed

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -48,9 +48,9 @@ function Navbar({ individual, links, setShowSideBar, showSideBar }) {
                     }>
                     <path strokeLinecap="round" strokeLinejoin="round" d="M18.75 19.5l-7.5-7.5 7.5-7.5m-6 15L5.25 12l7.5-7.5" />
                 </svg>
-                <Link to={notifications}>
-                    {
-                        notifications && (
+                {
+                    notifications && (
+                        <Link to={notifications}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 strokeWidth={1.5}
                                 stroke="currentColor"
@@ -60,9 +60,9 @@ function Navbar({ individual, links, setShowSideBar, showSideBar }) {
                                 }>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0M3.124 7.5A8.969 8.969 0 015.292 3m13.416 0a8.969 8.969 0 012.168 4.5" />
                             </svg>
-                        )
-                    }
-                </Link>
+                        </Link>
+                    )
+                }
                 {
                     attendence && (
                         <svg
@@ -79,9 +79,10 @@ function Navbar({ individual, links, setShowSideBar, showSideBar }) {
                         </svg>
                     )
                 }
-                <Link to={chat}>
-                    {
-                        chat && (
+
+                {
+                    chat && (
+                        <Link to={chat}>
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 strokeWidth={1.5}
                                 stroke="currentColor"
@@ -90,9 +91,9 @@ function Navbar({ individual, links, setShowSideBar, showSideBar }) {
                                     } cursor-pointer`}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
                             </svg>
-
-                        )
-                    } </Link>
+                        </Link>
+                    )
+                }
             </div>
             <div className="flex items-center">
                 {
