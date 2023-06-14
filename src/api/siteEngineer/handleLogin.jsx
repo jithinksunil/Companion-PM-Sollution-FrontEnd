91d0ce665b40c19589424295c8092196387d7fca
jithin k sunil <jithinksunil96@gmail.com>
2023-06-14 15:31:35 +0530

    subdomain config for backend done

--- src/api/siteEngineer/handleLogin.jsx
+++ src/api/siteEngineer/handleLogin.jsx
@@ -3,12 +3,12 @@ import { postApi } from "../axiosCalls";
 import { toast } from "react-toastify";
 import { setSiteEngineer } from "../../store/slices/SiteEngineerSlice";
 
-
 export const handleLogin = ({ formData, dispatch, navigate }) => {
   postApi("/siteengineer/login", formData, (response) => {
     const { data, verified, message } = response.data
     if (verified) {
-      Cookies.set('siteEngineerToken', response.data.token, {domain:`.${process.env.DOMAIN_NAME?.split('://')[1]?.split(':')[0]}`, expires: 7000,sameSite:'Lax'});
+      Cookies.set('siteEngineerToken', response.data.token, {domain:`.${process.env.REACT_APP_DOMAIN_NAME?.split('://')[1].split(':')[0]}`, expires: 7000,sameSite:'Lax'});
+
       dispatch(setSiteEngineer(data))
       navigate("/siteengineer/dashboard");
     }
--- src/api/superUser/handleLogin.jsx
+++ src/api/superUser/handleLogin.jsx
@@ -8,9 +8,12 @@ export const handleLogin = ({ formData, dispatch, navigate }) => {
     const { data, verified, message } = response.data
     if (verified) {
       // document.cookie=`superUserToken=${response.data.token};max-age=604800;SameSite=None;Secure`
-      Cookies.set('superUserToken', response.data.token, {domain:`.${process.env.DOMAIN_NAME?.split('://')[1]?.split(':')[0]}`, expires: 7000,sameSite:'Lax'});
+      Cookies.set('superUserToken', response.data.token, {domain:`.${process.env.REACT_APP_DOMAIN_NAME?.split('://')[1].split(':')[0]}`, expires: 7000,sameSite:'Lax'});
       dispatch(setSuperUser(data))
       navigate("/superuser/dashboard");
+
+
+
     }
     toast(message);
   });
